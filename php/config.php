5af4ea975fdf8731036c2c72cdda1d73c32d9a6a
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-20 18:19:53 +0200

    Added Access-Control-Allow headers

--- php/config.php
+++ php/config.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $host = "localhost:3306";
 $dbuser = "root";
 $dbpass = "";
