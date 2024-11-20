5c6b56becc9bff570dab3a30ec0bbc318c13a903
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-20 18:13:44 +0200

    Started AJAX & created XML object

--- scripts/signup.js
+++ scripts/signup.js
@@ -4,4 +4,10 @@ const errorText = document.querySelector(".error-txt");
 
 forms.forEach((form) => {
     form.onsubmit = (e) => {
-        e.preventDefault(); // Prevent form from submitting )}};
+        e.preventDefault(); // Prevent form from submitting
+
+        // Ajax start
+        let xhr = new XMLHttpRequest(); // Create XML Object
+        xhr.open("POST", "./php/signup.php", true);
+    };
+});
