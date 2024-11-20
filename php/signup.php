7ef1b40c1650f5b79c2ae5e0dc8b8f24b43ed87a
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-20 18:23:43 +0200

    Added validation for email

--- php/signup.php
+++ php/signup.php
@@ -12,4 +12,16 @@ $lname = mysqli_real_escape_string($connection, $_POST['lname']);
 $email = mysqli_real_escape_string($connection, $_POST['email']);
 $password = mysqli_real_escape_string($connection, $_POST['password']);
 
+if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
+
+    // Validate email
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        // Check if email already exists
+        $sql = mysqli_query($connection, "SELECT email FROM users WHERE email = '{$email}'");
+        if (mysqli_num_rows($sql) > 0) {
+            echo "$email - This email already exists!";
+        } else {
+            
+        }
 ?>
