61c423c66c2ec82f967554d4adeb39390ec7b5be
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-20 18:12:26 +0200

    Added function to prevent form from submitting default data

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,7 @@
 // Select all forms within the .signup container
 const forms = document.querySelectorAll(".signup form"); 
 const errorText = document.querySelector(".error-txt");
+
+forms.forEach((form) => {
+    form.onsubmit = (e) => {
+        e.preventDefault(); // Prevent form from submitting )}};
