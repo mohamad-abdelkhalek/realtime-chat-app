3a25d2bc6c632607eddd3a2054aa80b82760f24c
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-20 18:22:35 +0200

    Sanitized input data

--- php/signup.php
+++ php/signup.php
@@ -1,1 +1,15 @@
-<?php?>
+<?php
+session_start();
+// Allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain");
+
+include_once "config.php";
+
+// Sanitize input data
+$fname = mysqli_real_escape_string($connection, $_POST['fname']);
+$lname = mysqli_real_escape_string($connection, $_POST['lname']);
+$email = mysqli_real_escape_string($connection, $_POST['email']);
+$password = mysqli_real_escape_string($connection, $_POST['password']);
+
+?>
