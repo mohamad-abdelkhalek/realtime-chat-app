c6985a75f611c82fd7db906efe4677ae9e6cdf01
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-22 18:03:20 +0200

    Started AJAX & created new XML Object

--- scripts/login.js
+++ scripts/login.js
@@ -6,5 +6,8 @@ forms.forEach((form) => {
     form.onsubmit = (e) => {
         e.preventDefault(); // Prevent the form from submitting
 
+         // AJAX start
+         const xhr = new XMLHttpRequest(); // Create a new XMLHttpRequest object
+         xhr.open("POST", "./php/login.php", true);
 
     );}}
