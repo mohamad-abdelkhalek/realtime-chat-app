2a0d317ecbe807d6fc282854316cf3109f80d344
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-22 18:07:54 +0200

    Added cross-origin requests headers & included the connection file

---
+++ php/login.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+// Allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+include_once "config.php";
+
+$response = [];
+
+?>
