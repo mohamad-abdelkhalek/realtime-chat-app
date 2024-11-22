d0b02c34b690885c0d3c61818dbd918270d3a777
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-22 18:02:36 +0200

    Added prevent function to disallow default data

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,10 @@
 // Select all forms within the .login container
 const forms = document.querySelectorAll(".login form");
 const errorText = document.querySelector(".error-txt");
+
+forms.forEach((form) => {
+    form.onsubmit = (e) => {
+        e.preventDefault(); // Prevent the form from submitting
+
+
+    );}}
